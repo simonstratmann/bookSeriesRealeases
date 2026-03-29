afdd7fe37010794e2c40f9fd757602aeccfc3085
Simon Stratmann <stratmann.simon@gmail.com>
2026-03-29 16:17:12 +0200

    Allow all origins for cors

--- src/main/java/de/sist/bookseries/api/CorsConfig.java
+++ src/main/java/de/sist/bookseries/api/CorsConfig.java
@@ -10,7 +10,6 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
